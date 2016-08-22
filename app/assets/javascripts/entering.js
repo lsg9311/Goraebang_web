053daf2de50a1a797ca98db2e086a3e036d15281
lsg9311 <crit9411@naver.com>
2016-08-22 15:26:00 +0900

    0822 entering

--- app/assets/javascripts/entering.js
+++ app/assets/javascripts/entering.js
@@ -12,11 +12,14 @@ $(document).ready(function(){
 		var email=$("#user_email").val();
 		var pw=$("#user_password").val();
 		var send_data="user[email]="+email+"&user[password]="+pw;
-		$.getJSON({
+		$.ajax({
 			url: "http://52.78.113.43/json/login",
+			type: "POST",
+			dataType:"json",
 			data:send_data,
 			success: function(data){
 				if(data.result=="SUCCESS"){
+					alert("아이디나 비밀번호가 일치합니다.");
 					window.location.href=next_url;
 				}
 				else{
@@ -43,9 +46,11 @@ $(document).ready(function(){
 			return;
 		}
 		var send_data="user[email]="+email+"&user[name]="+name+"&user[password]="+pw+"&user[password_confirmation]="+pwc;
-		$.getJSON({
+		$.ajax({
 			url: "http://52.78.113.43/json/regist",
+			type: "POST",
 			data:send_data,
+			dataType:"json",
 			success: function(data){
 				if(data.result=="SUCCESS"){
 					alert("회원가입이 완료되었습니다.");
